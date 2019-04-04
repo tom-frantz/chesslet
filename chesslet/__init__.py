0d7018feaf340cf7375da8fe5c74cbf91d20795d
Black-Tomcat <tomcat.frantz@gmail.com>
2019-04-04 19:56:43 +1100

    Placed in dummy files around the game code

--- chesslet/__init__.py
+++ chesslet/__init__.py
@@ -1,1 +1,8 @@
-# This can just be an empty file
+"""
+CONTENTS OF THIS MODULE:
+
+board.py: The file that holds the Board class, and hosts the game.
+piece.py: A piece on the board. Self explanatory.
+
+movesets.json: A JSON file that holds the movesets for each type of piece.
+"""
