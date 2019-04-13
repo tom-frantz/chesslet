bb909f7af3e7ce085269aedea9b34f9d59bf3b68
Black-Tomcat <tomcat.frantz@gmail.com>
2019-04-13 18:17:48 +1000

    Initial code for the piece

--- chesslet/piece.py
+++ chesslet/piece.py
@@ -1,2 +1,14 @@
 class Piece:
-    pass
+    move_sets = {
+        "Rook": [(1, 1), ...],
+        "Knight": [(1, 1), ...],
+        "Bishop": [(1, 1), ...]
+    }
+
+    def __init__(self, initial_combinations):
+        self.combination_states = {
+            "Rook": 0,
+            "Knight": 0,
+            "Bishop": 0,
+            **initial_combinations
+        }
