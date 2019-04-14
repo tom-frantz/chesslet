c9cee134d8bcc6f9dbfc89b37f457277f87956b6
Flynn Swainston-Calcutt <49269260+FlynnSC@users.noreply.github.com>
2019-04-14 18:14:46 +1000

    Added Point class

---
+++ chesslet/point.py
@@ -0,0 +1,13 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __add__(self, point):
+        return Point(self.x + point.x, self.y + point.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return "(%d, %d)" % (self.x, self.y)
