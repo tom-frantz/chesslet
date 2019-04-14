ec95edf74af1b1fb100acc39f82c6813ca5336e4
Grace <glkerr07@gmail.com>
2019-04-14 18:40:05 +1000

    Added move_piece to Session and extra tests

--- chesslet/session.py
+++ chesslet/session.py
@@ -1,7 +1,16 @@
 from chesslet.player import PlayerNotLoggedIn
+from chesslet.board import Board
+from chesslet.point import Point
+
+# -Session class has a function called by the server, which takes from pos,
+#  to pos, player id, and a name of a piece if it's being split off
 
 
 class PlayerAlreadyAddedException(Exception):
+    pass
+
+
+class PlayerNotAddedException(Exception):
     pass
 
 
@@ -9,6 +18,7 @@ class Session:
     def __init__(self, uuid):
         self.uuid = uuid
         self.players = {}
+        self.board = Board(self.uuid)
 
     def add_player(self, player):
         # if expression
@@ -20,5 +30,17 @@ class Session:
         else:
             raise PlayerNotLoggedIn("Cannot allow a not logged in player into game")
 
-    def recieve_event(self):
-        pass
+    def remove_player(self, player):
+        if player.uuid in self.players:
+            del self.players[player.uuid]
+        else:
+            raise PlayerNotLoggedIn("Cannot remove a player not added to game")
+
+    # Simple move piece without being able to split or combine piece
+    def move_piece(self, player, curr_pos_x, curr_pos_y, new_pos_x, new_pos_y):
+        if player.uuid not in self.players:
+            raise PlayerNotAddedException("Player not added cannot move piece")
+
+        curr_pos = Point(curr_pos_x, curr_pos_y)
+        new_pos = Point(new_pos_x, new_pos_y)
+        self.board.move_piece(player, curr_pos, new_pos)
--- tests/test_session.py
+++ tests/test_session.py
@@ -2,7 +2,7 @@ import unittest
 from functools import partial
 
 from chesslet.session import Session, PlayerAlreadyAddedException
-from chesslet.player import Player
+from chesslet.player import Player, PlayerNotLoggedIn
 
 
 class SessionTest(unittest.TestCase):
@@ -44,6 +44,32 @@ class SessionTest(unittest.TestCase):
 
         # Check to see if session.players is equivalent to an array with the player in it
         self.assertEqual(session.players, {"a": player})
+
+    def test_remove_player(self):
+        session = Session("1234")
+        player = Player(
+            uuid="a",
+            password="b",
+            account_name="c"
+        )
+        player.logged_in = True
+        session.add_player(player)
+
+        session.remove_player(player)
+
+        self.assertEqual(session.players, {})
+
+    def test_remove_player_fail(self):
+        session = Session("1234")
+        player = Player(
+            uuid="a",
+            password="b",
+            account_name="c"
+        )
+        player.logged_in = True
+
+        self.assertRaises(PlayerNotLoggedIn, session.remove_player, player)
+
 
 
 if __name__ == '__main__':
