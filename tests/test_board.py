a5a38090e33322e61f8c0e38841f9b8f87a796ab
Black-Tomcat <tomcat.frantz@gmail.com>
2019-04-27 13:30:10 +1000

    Cleaned up the test_board tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -6,110 +6,99 @@ from chesslet.board import InvalidMoveException
 from chesslet.piece import Piece
 from chesslet.player import Player
 from chesslet.point import Point
+from chesslet.session import Session
 
 BOARD = Board(True)
 
 
+
+
+
 class BoardTest(unittest.TestCase):
+    def move_from(self, from_point):
+        def inside(x, y):
+            return partial(self.board.move_piece, True, from_point, Point(x, y))
+        return inside
+
+    def create_player_piece(self, piece, player_pieces, x, y):
+        self.board.board[x][y] = piece
+        player_pieces.append(piece)
+        return piece
+
     def setUp(self):
         self.player_1 = Player("abc", "unsafe", "tom")
         self.player_2 = Player("yeet", "unsafe", "jashdjas")
-        self.board = Board(True)
-
+        self.board = Board(Session("abc"), True)
+    
     def test_move_rook(self):
-        piece = Piece({"Rook": 1})
-        self.board.board[0][0] = piece
-        self.board.player_1_pieces.append(self.board.board[0][0])
-        self.board.board[1][0] = Piece({"Knight": 1})
+        piece = self.create_player_piece(Piece({"Rook": 1}), self.board.player_1_pieces, 0, 0)
+        self.create_player_piece(Piece({"Knight": 1}), self.board.player_2_pieces, 1, 0)
+        rook_move = self.move_from(Point(0, 0))
 
         # Tests invalid movement outside of the board
-        outside_move = partial(self.board.move_piece, True, Point(0, 0), Point(-1, 0))
-        self.assertRaises(InvalidMoveException, outside_move)
+        self.assertRaises(InvalidMoveException, rook_move(-1, 0))
 
         # Tests invalid diagonal movement
-        diagonal_move = partial(self.board.move_piece, True, Point(0, 0), Point(1, 1))
-        self.assertRaises(InvalidMoveException, diagonal_move)
+        self.assertRaises(InvalidMoveException, rook_move(1, 1))
 
         # Tests invalid movement over another piece
-        over_move = partial(self.board.move_piece, True, Point(0, 0), Point(2, 0))
-        self.assertRaises(InvalidMoveException, over_move)
+        self.assertRaises(InvalidMoveException, rook_move(2, 0))
 
         # Tests invalid orthogonal movement greater than 2 spaces
-        far_move = partial(self.board.move_piece, True, Point(0, 0), Point(0, 4))
-        self.assertRaises(InvalidMoveException, far_move)
+        self.assertRaises(InvalidMoveException, rook_move(0, 4))
 
         # Tests valid orthogonal movement
-        self.board.move_piece(True, Point(0, 0), Point(0, 2))
+        rook_move(0, 2)()
         self.assertEqual(self.board.board[0][2], piece)
-    
+
     def test_move_bishop(self):
-        piece = Piece({"Bishop": 1})
-
-        self.board.board[3][0] = piece
-        self.board.player_1_pieces.append(self.board.board[3][0])
-
-        self.board.board[4][1] = Piece({"Rook": 1})
-        self.board.player_2_pieces.append(self.board.board[4][1])
+        piece = self.create_player_piece(Piece({"Bishop": 1}), self.board.player_1_pieces, 3, 0)
+        self.create_player_piece(Piece({"Rook": 1}), self.board.player_2_pieces, 4, 1)
+        bishop_move = self.move_from(from_point=Point(3, 0))
 
         # Tests invalid orthogonal movement
-        part = partial(self.board.move_piece, True, Point(3, 0), Point(3, 2))
-        self.assertRaises(InvalidMoveException, part)
+        self.assertRaises(InvalidMoveException, bishop_move(3, 2))
 
         # Tests invalid movement over another piece
-        part = partial(self.board.move_piece, True, Point(3, 0), Point(5, 2))
-        self.assertRaises(InvalidMoveException, part)
+        self.assertRaises(InvalidMoveException, bishop_move(5, 2))
 
         # Tests invalid diagonal movement greater than 2 spaces
-        part = partial(self.board.move_piece, True, Point(3, 0), Point(0, 3))
-        self.assertRaises(InvalidMoveException, part)
+        self.assertRaises(InvalidMoveException, bishop_move(0, 3))
 
         # Tests valid orthogonal movement
-        self.board.move_piece(True, Point(3, 0), Point(1, 2))
+        bishop_move(1, 2)()
         self.assertEqual(self.board.board[1][2], piece)
 
     def test_move_knight(self):
-        piece = Piece({"Knight": 1})
-        self.board.board[3][0] = piece
-        self.board.player_1_pieces.append(self.board.board[3][0])
-
-        self.board.board[3][1] = Piece({"Bishop": 1})
-        self.board.player_2_pieces.append(self.board.board[3][1])
-        self.board.board[3][2] = Piece({"Bishop": 1})
-        self.board.player_2_pieces.append(self.board.board[3][2])
-        self.board.board[4][1] = Piece({"Bishop": 1})
-        self.board.player_2_pieces.append(self.board.board[4][1])
+        piece = self.create_player_piece(Piece({"Knight": 1}), self.board.player_1_pieces, 3, 0)
+        create_bishop = partial(self.create_player_piece, Piece({"Bishop": 1}), self.board.player_2_pieces)
+        create_bishop(3, 1)
+        create_bishop(3, 2)
+        create_bishop(4, 1)
+        knight_move = self.move_from(Point(3, 0))
 
         # Tests invalid non-L-shaped movement
-        part = partial(self.board.move_piece, True, Point(3, 0), Point(5, 0))
-        self.assertRaises(InvalidMoveException, part)
+        self.assertRaises(InvalidMoveException, knight_move(5, 0))
 
         # Tests valid L-shaped movement
-        self.board.move_piece(True, Point(3, 0), Point(1, 1))
+        knight_move(1, 1)()
         self.assertEqual(self.board.board[1][1], piece)
         self.board.move_piece(True, Point(1, 1), Point(3, 0))
 
         # Tests valid L-shaped movement over other pieces
-        self.board.move_piece(True, Point(3, 0), Point(4, 2))
+        knight_move(4, 2)()
         self.assertEqual(self.board.board[4][2], piece)
 
     def test_combine_piece(self):
-        piece = Piece({"Bishop": 1})
-        other_piece = Piece({"Rook": 1})
-
-        self.board.board[3][0] = piece
-        self.board.player_1_pieces.append(self.board.board[3][0])
-
-        self.board.board[4][1] = other_piece
-        self.board.player_1_pieces.append(self.board.board[4][1])
+        self.create_player_piece(Piece({"Bishop": 1}), self.board.player_1_pieces, 3, 0)
+        self.create_player_piece(Piece({"Rook": 1}), self.board.player_1_pieces, 4, 1)
 
         # Tests combining the bishop into the rook
         self.board.move_piece(True, Point(3, 0), Point(4, 1))
-        self.assertEqual(other_piece.combination_state, {"Rook": 1, "Bishop": 1, "Knight": 0})
+        self.assertEqual(self.board.board[4][1].combination_state, {"Rook": 1, "Bishop": 1, "Knight": 0})
 
     def test_move_combined_piece(self):
-        piece = Piece({"Rook": 1, "Bishop": 1})
-        self.board.board[3][0] = piece
-        self.board.player_1_pieces.append(self.board.board[3][0])
+        piece = self.create_player_piece(Piece({"Rook": 1, "Bishop": 1}), self.board.player_1_pieces, 3, 0)
 
         # Tests rook movement
         self.board.move_piece(True, Point(3, 0), Point(3, 2))
@@ -120,9 +109,7 @@ class BoardTest(unittest.TestCase):
         self.assertEqual(self.board.board[5][4], piece)
 
     def test_split_piece(self):
-        piece = Piece({"Rook": 1, "Bishop": 1})
-        self.board.board[3][0] = piece
-        self.board.player_1_pieces.append(self.board.board[3][0])
+        self.create_player_piece(Piece({"Rook": 1, "Bishop": 1}), self.board.player_1_pieces, 3, 0)
 
         # Splits rook out
         self.board.move_piece(True, Point(3, 0), Point(3, 2), {"Rook": 1})
@@ -132,6 +119,7 @@ class BoardTest(unittest.TestCase):
         # Test invalid rook movement by the bishop
         part = partial(self.board.move_piece, True, Point(3, 0), Point(3, 1))
         self.assertRaises(InvalidMoveException, part)
+
 
 if __name__ == '__main__':
     unittest.main()
